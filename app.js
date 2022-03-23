2a2ad9e7f2347346038f47453d917f5fbf883e43
Benedij <97602321+Benedij@users.noreply.github.com>
2022-03-23 19:18:30 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import Movies from "./components/Movies";
+import Movie from "./components/Movie";
+import MovieService from "./components/MovieService";
+//<MovieService
+
+export default function App() {
+  return (
+    <div>
+      <Movie />
+    </div>
+  );
+}
